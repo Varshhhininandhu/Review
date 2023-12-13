c2e7aa82eb51cdbfd63a856f407572457c9312c5
Varshhhininandhu <153708072+Varshhhininandhu@users.noreply.github.com>
2023-12-13 12:09:24 +0530

    Add files via upload

---
+++ day1/LoginForm1.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import'./LoginForm1.css'
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
+
+  return (
+    <div className="login-container">
+      <form className="login-form">
+        <h2>Login</h2>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+      
+    </div>
+  );
+};
+export default Login;
